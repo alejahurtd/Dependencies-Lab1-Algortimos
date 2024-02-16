178f864b0c5cea5ccf9f4328e80cd0b1a7e1f6b5
alejahurtd <mairahurtado930@gmail.com>
2024-02-16 15:50:04 -0500

    pruebas

--- detalles.js
+++ detalles.js
@@ -2,6 +2,6 @@ const isHex = require('is-hex-color-code');
 
 console.log(isHex('#FFFFFF')); // Output: true
 console.log(isHex('#123abc')); // Output: true
-console.log(isHex('invalid')); // Output: false
+console.log(isHex('#FFFFFF')); // Output: false
 console.log(isHex('#HHJJZZ')); // Output: false
 console.log(isHex('#ACBD')); // Output: false
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Modulo
 const calculator = require('birthday-calculator');
 
-const date = new Date('1990,10,01');
+const date = new Date('2001,09,30');
 const result = calculator.getDayWhenBorn(date);
 console.log(result);
+
+// Las dependencia que instale es la de dirthday-calculator, lo que hace es que me calcula con la fecha que le
+// proporciono el dia en el cual la persona nació
